ad92c29b8ed0bc924e18c4505ae5e765854b568f
Walter Daniel Carrizo <walterdcarrizo.19@gmail.com>
2025-07-07 16:29:38 -0300

    add: Project setup

---
+++ src/main/java/dev/purple/mypos/MyPOS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package dev.purple.mypos;
+
+/**
+ *
+ * @author walle
+ */
+public class MyPOS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
